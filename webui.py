524bc94dc922f302aa3b5ee02e64f54b6520c328
Snychng <snychng@gmail.com>
2024-01-09 22:40:24 +0800

    update mainform

--- webui.py
+++ webui.py
@@ -1,32 +1,85 @@
 import gradio as gr
+import librosa  # 语音数据格式返回
+import time
+import httpx
+import gradio as gr
+from openai import OpenAI
+import yaml
 
-# 定义与GPT模型交互的函数
-def gpt_response(input_text, onload_checkbox, slider):
-    # 实际应用中应从GPT模型获取响应
-    return "这是GPT的响应示例。"
+# 加载config文件
+with open('config.yml', 'r') as config_file:
+    config = yaml.safe_load(config_file)
 
-# 创建GPT API接口展示框和用户输入框
-# 在这个例子中，我们直接使用了gpt_response函数作为Gradio接口的fn参数
+http_client = httpx.Client(
+    base_url=f"https://api.openai.com/v1",
+    proxies={
+        'http://': config['http_proxy'],
+        'https://': config['https_proxy']
+    }
+)
+
+ai_client = OpenAI(
+    api_key=config['openai_api_key'],
+    http_client=http_client
+)
+
+# 定义与OpenAI GPT模型交互的函数
+def slow_echo(message, history):
+    messages = []
+    messages.append({"role": "system", "content": config['gpt_system']})
+    # 构建历史消息列表
+    for ht in history:
+        if ht[1]:            
+            messages.append({"role": "user", "content": ht[0]})
+            messages.append({"role": "assistant", "content": ht[1]})
+    messages.append({"role": "user", "content": message})
+
+    # 请求OpenAI GPT模型生成响应
+    response = ai_client.chat.completions.create(
+        model="gpt-4-1106-preview",
+        messages=messages,
+        temperature=0,
+        stream=True
+    )
+
+    start_time = time.time()
+    collected_chunks = []
+    collected_messages = []
+    try:
+        for chunk in response:
+            chunk_time = time.time() - start_time
+            collected_chunks.append(chunk)
+            chunk_message = chunk.choices[0].delta.content
+            if chunk_message:
+                collected_messages.append(chunk_message)
+            print(f"Message received after {chunk_time:.2f} s: {chunk_message}")
+            full_reply_content = ''.join(collected_messages)
+            yield full_reply_content
+    except Exception as e:
+        full_reply_content = '意外故障'
+        print(f"Error: {e}")
+        yield full_reply_content
+
+
+with open('./static/css/ui.css', 'r') as css_file:
+    ui_css = css_file.read()
 
 # 定义布局
-with gr.Blocks() as demo:
+with gr.Blocks(theme=gr.themes.Soft(), css=ui_css) as demo:
     with gr.Row():
-        gr.Markdown("# 予成AI数字人")
-    with gr.Row():
-        gr.Image(value="./static/pic/pic2.jpg", label="Image Demo")
-    with gr.Row():
-        input_text = gr.Textbox(label="输入您的问题", placeholder="请输入您的问题", lines=2)
-        output_text = gr.Textbox(label="GPT的响应", lines=4)
-        # input_text.change(fn=gpt_response, inputs=input_text, outputs=output_text)
-    with gr.Row():
-        submit_button = gr.Button("提交")  # 创建提交按钮
-        stop_button = gr.Button("停止", variant="secondary")  # 创建停止按钮
-    with gr.Row():
-        gr.Interface(
-            fn=gpt_response,
-            inputs=["text", "checkbox", gr.Slider(0, 100)],
-            outputs=["text", "number"],
-        )
+            gr.Markdown("# <center> 予成")
+    with gr.Tab("文本生成对话") as text2text_tab:
+        with gr.Row():
+            gr.Markdown("## 文本生成对话")
+        with gr.Row():
+            gr.ChatInterface(slow_echo)
+        with gr.Row():
+            gr.Markdown("<br> 这是一个基于Gradio的ChatGPT-Webui")
+    with gr.Tab("语音生成对话") as text2voice_tab:
+        with gr.Row():
+            gr.Markdown("## 语音生成对话")
+
+demo.queue()
 
 # 运行界面，指定端口为 8888
-demo.launch(server_port=8888)
+demo.launch(server_name='0.0.0.0', server_port=8888)
